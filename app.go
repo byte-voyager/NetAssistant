9063d32828af28535345968590cec76c2ef4e67a
baloneo <alonebo.zhou@gmail.com>
2020-06-19 09:34:04 +0800

    update

--- app.go
+++ app.go
@@ -32,35 +32,35 @@ type NetAssistantApp struct {
 	connList  []net.Conn
 	fileName  string
 
-	appWindow             *gtk.ApplicationWindow // app 主窗口
-	combProtoType         *gtk.ComboBoxText      // 服务类型下拉框
-	entryIP               *gtk.Entry             // IP地址
-	entryPort             *gtk.Entry             // 端口
-	btnConnect            *gtk.Button            // 连接按钮
-	btnClearRecvDisplay   *gtk.Button            // 清空接收区
-	btnClearSendDisplay   *gtk.Button            // 清空发送区
-	labelStatus           *gtk.Label             // 当前状态提示
-	labelSendCount        *gtk.Label             // 发送计数
-	labelReceveCount      *gtk.Label             // 接收计数
-	btnCleanCount         *gtk.Button            // 复位计数按钮
-	tvDataReceive         *gtk.TextView          // 数据接收区
-	swDataRec             *gtk.ScrolledWindow    // 数据接收区的滚动窗口
-	tvDataSend            *gtk.TextView          // 数据发送区
-	btnSend               *gtk.Button            // 发送消息按钮
-	entryCurAddr          *gtk.Entry             // 当前监听地址
-	entryCurPort          *gtk.Entry             // 当前监听端口
-	cbHexDisplay          *gtk.CheckButton       // 16进制显示
-	cbPauseDisplay        *gtk.CheckButton       // 暂停显示
-	cbDisplayDate         *gtk.CheckButton       // 接收显示日期且换行
-	cbDataSourceCycleSend *gtk.CheckButton       // 数据循环发送
-	cbSendByHex           *gtk.CheckButton       // 数据以16进制发送
-	tbReceData            *gtk.TextBuffer        //接收区buffer
-	tbSendData            *gtk.TextBuffer        // 发送区buffer
-	entryCycleTime        *gtk.Entry             // 持续发送数据的间隔
-	cbAutoCleanAfterSend  *gtk.CheckButton       // 发送后清空
-	cbReceive2File        *gtk.CheckButton       // 接收转向文件
-	btnSaveData           *gtk.Button            // 保存数据到文件
-	btnLoadData           *gtk.Button            // 从文件加载数据按钮
+	appWindow             *gtk.ApplicationWindow
+	combProtoType         *gtk.ComboBoxText
+	entryIP               *gtk.Entry
+	entryPort             *gtk.Entry
+	btnConnect            *gtk.Button
+	btnClearRecvDisplay   *gtk.Button
+	btnClearSendDisplay   *gtk.Button
+	labelStatus           *gtk.Label
+	labelSendCount        *gtk.Label
+	labelReceveCount      *gtk.Label
+	btnCleanCount         *gtk.Button
+	tvDataReceive         *gtk.TextView
+	swDataRec             *gtk.ScrolledWindow
+	tvDataSend            *gtk.TextView
+	btnSend               *gtk.Button
+	entryCurAddr          *gtk.Entry
+	entryCurPort          *gtk.Entry
+	cbHexDisplay          *gtk.CheckButton
+	cbPauseDisplay        *gtk.CheckButton
+	cbDisplayDate         *gtk.CheckButton
+	cbDataSourceCycleSend *gtk.CheckButton
+	cbSendByHex           *gtk.CheckButton
+	tbReceData            *gtk.TextBuffer
+	tbSendData            *gtk.TextBuffer
+	entryCycleTime        *gtk.Entry
+	cbAutoCleanAfterSend  *gtk.CheckButton
+	cbReceive2File        *gtk.CheckButton
+	btnSaveData           *gtk.Button
+	btnLoadData           *gtk.Button
 	labelLocalAddr        *gtk.Label
 	labelLocalPort        *gtk.Label
 	cbAppendNewLine       *gtk.CheckButton
@@ -118,7 +118,7 @@ func (app *NetAssistantApp) update(recvStr string) {
 
 	iter := app.tbReceData.GetEndIter()
 	app.tbReceData.Insert(iter, recvStr)
-	app.labelReceveCount.SetText("接收计数：" + strconv.Itoa(app.receCount))
+	app.labelReceveCount.SetText("Recv Count:" + strconv.Itoa(app.receCount))
 	app.tbReceData.CreateMark("end", iter, false)
 	mark := app.tbReceData.GetMark("end")
 
@@ -128,23 +128,22 @@ func (app *NetAssistantApp) update(recvStr string) {
 
 func (app *NetAssistantApp) updateSendCount(count int) {
 	app.sendCount += count
-	app.labelSendCount.SetText("发送计数：" + strconv.Itoa(app.sendCount))
+	app.labelSendCount.SetText("Send Count:" + strconv.Itoa(app.sendCount))
 }
 
 func (app *NetAssistantApp) handler(conn net.Conn) {
-	defer conn.Close() // 关闭连接
+	defer conn.Close() // close connection
 
 	for {
 		reader := bufio.NewReader(conn)
 		var buf [2048]byte
-		n, err := reader.Read(buf[:]) // 读取数据
+		n, err := reader.Read(buf[:])
 		if err != nil {
-			log.Println("从客户端读取数据异常，关闭此连接:", err)
+			log.Println("close connection:", err)
 			_, ok := conn.(*net.UDPConn)
 			if !ok {
-				log.Println("不是net.UDPConn")
 				ss := conn.RemoteAddr().String()
-				tips := fmt.Sprintf(`<span foreground="pink">😄连接被关闭: %s </span>`, ss)
+				tips := fmt.Sprintf(`<span foreground="pink">😄connection closed: %s </span>`, ss)
 				glib.IdleAdd(func() {
 					app.labelStatus.SetMarkup(tips)
 				})
@@ -153,7 +152,6 @@ func (app *NetAssistantApp) handler(conn net.Conn) {
 			for index, connItem := range app.connList {
 				if conn.LocalAddr().String() == connItem.LocalAddr().String() {
 					app.connList = append(app.connList[:index], app.connList[index+1:]...)
-					log.Println("已经将无效的连接移除")
 				}
 			}
 			return
@@ -174,11 +172,10 @@ func init() {
 func (app *NetAssistantApp) onBtnCleanCount() {
 	app.receCount = 0
 	app.sendCount = 0
-	app.labelReceveCount.SetText("接收计数：0")
-	app.labelSendCount.SetText("发送计数：0")
+	app.labelReceveCount.SetText("Recv Count:0")
+	app.labelSendCount.SetText("Send Count:0")
 }
 
-// onCbReceive2File 点击接收转向文件时调用
 func (app *NetAssistantApp) onCbReceive2File() {
 	if app.cbReceive2File.GetActive() {
 		dialog, _ := gtk.FileChooserNativeDialogNew("Select File", app.appWindow, gtk.FILE_CHOOSER_ACTION_OPEN, "Select", "Cancel")
@@ -192,7 +189,7 @@ func (app *NetAssistantApp) onCbReceive2File() {
 }
 
 func (app *NetAssistantApp) onBtnLoadData() {
-	log.Println("btn load data")
+
 	dialog, _ := gtk.FileChooserNativeDialogNew("Select File", app.appWindow, gtk.FILE_CHOOSER_ACTION_OPEN, "Select", "Cancel")
 	res := dialog.Run()
 	if res == int(gtk.RESPONSE_ACCEPT) {
@@ -235,12 +232,12 @@ func (app *NetAssistantApp) updateAllStatus(msg, curIP, curPort string) {
 func (app *NetAssistantApp) createConnect(serverType int, strIP, strPort string) error {
 	addr := strIP + ":" + strPort
 	if serverType == 0 { // TCP Client
-		conn, err := net.Dial("tcp", addr) // 创建连接
+		conn, err := net.Dial("tcp", addr)
 		if err == nil {
-			go app.handler(conn)    // 监听数据
-			app.addConnection(conn) // 加到连接列表
+			go app.handler(conn)
+			app.addConnection(conn)
 			locallConnInfo := strings.Split(conn.LocalAddr().String(), ":")
-			app.updateAllStatus("TCP Client连接成功", locallConnInfo[0], locallConnInfo[1])
+			app.updateAllStatus("TCP client connection succeeds", locallConnInfo[0], locallConnInfo[1])
 
 		} else {
 			app.updateAllStatus(err.Error(), "", "")
@@ -254,16 +251,16 @@ func (app *NetAssistantApp) createConnect(serverType int, strIP, strPort string)
 
 		if err == nil {
 			log.Println("listen failed, err:", err)
-			app.updateStatus("TCP Server连接成功")
+			app.updateStatus("TCP server connection succeeds")
 			go func() {
 				for {
 					conn, err := listen.Accept() // 等待客户端
 					if err != nil {
-						log.Println("accept 失败, err:", err, "退出监听")
+						log.Println("accept err:", err)
 						return
 					}
 					ss := conn.RemoteAddr().String()
-					tips := fmt.Sprintf(`<span foreground="green">新的连接:%s </span>`, ss)
+					tips := fmt.Sprintf(`<span foreground="green">new connection:%s </span>`, ss)
 					glib.IdleAdd(func() {
 						app.labelStatus.SetMarkup(tips)
 					})
@@ -273,7 +270,7 @@ func (app *NetAssistantApp) createConnect(serverType int, strIP, strPort string)
 				}
 			}()
 
-			app.updateAllStatus("TCP Server连接成功", strIP, strPort)
+			app.updateAllStatus("TCP server connection succeeds", strIP, strPort)
 
 			app.listener = listen
 		} else {
@@ -288,7 +285,7 @@ func (app *NetAssistantApp) createConnect(serverType int, strIP, strPort string)
 			go app.handler(conn)
 			app.addConnection(conn)
 			localConnInfo := strings.Split(conn.LocalAddr().String(), ":")
-			app.updateAllStatus("UDP Client连接成功", localConnInfo[0], localConnInfo[1])
+			app.updateAllStatus("UDP client connection succeeds", localConnInfo[0], localConnInfo[1])
 		} else {
 			app.updateStatus(err.Error())
 			return err
@@ -299,16 +296,16 @@ func (app *NetAssistantApp) createConnect(serverType int, strIP, strPort string)
 	if serverType == 3 { // UDP Server
 		address, err := net.ResolveUDPAddr("udp4", addr)
 		if err != nil {
-			app.updateStatus("UDP Server连接失败：" + err.Error())
+			app.updateStatus("UDP server connection succeeds" + err.Error())
 		} else {
 			udpConn, err := net.ListenUDP("udp4", address)
 			if err == nil {
 				go app.handler(udpConn)
 				app.addConnection(udpConn)
 				localConnInfo := strings.Split(udpConn.LocalAddr().String(), ":")
-				app.updateAllStatus("UDP Server连接成功", localConnInfo[0], localConnInfo[1])
-				app.labelLocalAddr.SetLabel("目标UDP地址")
-				app.labelLocalPort.SetLabel("目标UDP端口")
+				app.updateAllStatus("UDP server connection succeeds", localConnInfo[0], localConnInfo[1])
+				app.labelLocalAddr.SetLabel("Taget UDP IP")
+				app.labelLocalPort.SetLabel("Taget UDP Port")
 				app.entryCurAddr.SetEditable(true)
 				app.entryCurAddr.SetText("")
 				app.entryCurPort.SetEditable(true)
@@ -335,15 +332,15 @@ func (app *NetAssistantApp) disconnect(serverType int) error {
 	}
 
 	if serverType == 3 {
-		app.labelLocalAddr.SetLabel("当前地址")
-		app.labelLocalPort.SetLabel("当前端口")
+		app.labelLocalAddr.SetLabel("Local IP")
+		app.labelLocalPort.SetLabel("Local Port")
 		app.entryCurAddr.SetEditable(false)
 		app.entryCurAddr.SetText("")
 		app.entryCurPort.SetEditable(false)
 		app.entryCurPort.SetText("")
 	}
 
-	app.updateStatus("等待连接")
+	app.updateStatus("waiting connection")
 	app.connList = []net.Conn{}
 	return nil
 }
@@ -407,7 +404,6 @@ func (app *NetAssistantApp) onBtnSend() {
 	}
 
 	if app.cbDataSourceCycleSend.GetActive() {
-		// 数据是否循环发送
 		app.btnSend.SetLabel("Stop")
 		strCycleTime, err := app.entryCycleTime.GetText()
 		if err != nil {
@@ -430,10 +426,9 @@ func (app *NetAssistantApp) onBtnSend() {
 							strPort, _ := app.entryCurPort.GetText()
 							address, err := net.ResolveUDPAddr("udp4", strIP+":"+strPort)
 							if err == nil {
-								log.Println("是udp")
 								cc.WriteToUDP(sendData, address)
 							} else {
-								log.Println("udp目标地址解析错误")
+								log.Println(err)
 							}
 
 						} else {
@@ -443,7 +438,7 @@ func (app *NetAssistantApp) onBtnSend() {
 					if len(app.connList) == 0 {
 
 						glib.IdleAdd(func() {
-							app.labelStatus.SetText("没有TCP连接了")
+							app.labelStatus.SetText("there's no connection")
 							app.btnSend.SetLabel("Send")
 						})
 						break END
@@ -496,21 +491,22 @@ func (app *NetAssistantApp) doActivate(application *gtk.Application) {
 	app.appWindow.SetIcon(buf)
 
 	app.appWindow.SetBorderWidth(10)
-	app.appWindow.SetTitle("网络调试助手")
+	app.appWindow.SetTitle("Network Assistant")
 
-	// 总体容器
+	// container
 	windowContainer, _ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 10)
 	windowContainerMiddle, _ := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 10)
 	windowContainerLeft, _ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 10)
 	windowContainerRight, _ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 10)
 	windowContainerBottom, _ := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 10)
 
-	// 左边的布局
-	frame, _ := gtk.FrameNew("网络设置")
+	// Left box
+	// type of service frame
+	frame, _ := gtk.FrameNew("Network Settings")
 	frame.SetLabelAlign(0.1, 0.5)
-	verticalBox, _ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 10)
-	// 1 服务类型的组件
-	labelProtType, _ := gtk.LabelNew("服务类型")
+	verticalBox, _ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 10) //
+	frame.Add(verticalBox)
+	labelProtType, _ := gtk.LabelNew("Type Of Service")
 	labelProtType.SetXAlign(0)
 	app.combProtoType, _ = gtk.ComboBoxTextNew()
 	app.combProtoType.AppendText("TCP Client")
@@ -518,44 +514,39 @@ func (app *NetAssistantApp) doActivate(application *gtk.Application) {
 	app.combProtoType.AppendText("UDP Client")
 	app.combProtoType.AppendText("UDP Server")
 	app.combProtoType.SetActive(0)
-	// 添加到容器
 	verticalBox.PackStart(labelProtType, false, false, 0)
 	verticalBox.PackStart(app.combProtoType, false, false, 0)
-	// 2 服务器IP设置
-	labelIP, _ := gtk.LabelNew("IP设置")
+	labelIP, _ := gtk.LabelNew("IP")
 	labelIP.SetXAlign(0)
 	app.entryIP, _ = gtk.EntryNew()
 	app.entryIP.SetText("127.0.0.1")
 	verticalBox.PackStart(labelIP, false, false, 0)
 	verticalBox.PackStart(app.entryIP, false, false, 0)
-	// 3 服务器端口
-	labelPort, _ := gtk.LabelNew("端口设置")
+	labelPort, _ := gtk.LabelNew("Port")
 	labelPort.SetXAlign(0)
 	app.entryPort, _ = gtk.EntryNew()
 	app.entryPort.SetText("8003")
 	verticalBox.PackStart(labelPort, false, false, 0)
 	verticalBox.PackStart(app.entryPort, false, false, 0)
-	// 4 连接按钮
 	app.btnConnect, _ = gtk.ButtonNewWithLabel("Connect")
 	app.btnConnect.Connect("clicked", app.onBtnConnect)
 	verticalBox.PackStart(app.btnConnect, false, false, 0)
 
-	// 5 两个切换按钮
+	// Recv Settings, Send Settings
 	notebookTab, _ := gtk.NotebookNew()
-	label1, _ := gtk.LabelNew("接收设置")
-	label2, _ := gtk.LabelNew("发送设置")
-
-	// 接收设置
+	label1, _ := gtk.LabelNew("Recv Settings")
+	label2, _ := gtk.LabelNew("Send Settings")
+	//  Recv Settings
 	frame1ContentBox, _ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 10)
-	app.cbReceive2File, _ = gtk.CheckButtonNewWithLabel("接收转向文件")
+	app.cbReceive2File, _ = gtk.CheckButtonNewWithLabel("Receive steering files")
 	app.cbReceive2File.Connect("toggled", app.onCbReceive2File)
-	app.cbDisplayDate, _ = gtk.CheckButtonNewWithLabel("显示时间且换行")
-	app.cbHexDisplay, _ = gtk.CheckButtonNewWithLabel("十六进制显示")
-	app.cbPauseDisplay, _ = gtk.CheckButtonNewWithLabel("暂停接收")
+	app.cbDisplayDate, _ = gtk.CheckButtonNewWithLabel("Show time and wrap")
+	app.cbHexDisplay, _ = gtk.CheckButtonNewWithLabel("Hex display")
+	app.cbPauseDisplay, _ = gtk.CheckButtonNewWithLabel("Pause display")
 	btnHboxContainer, _ := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 10)
-	app.btnSaveData, _ = gtk.ButtonNewWithLabel("保存数据")
+	app.btnSaveData, _ = gtk.ButtonNewWithLabel("Save")
 	app.btnSaveData.Connect("clicked", app.onBtnSaveData)
-	app.btnClearRecvDisplay, _ = gtk.ButtonNewWithLabel("清空显示")
+	app.btnClearRecvDisplay, _ = gtk.ButtonNewWithLabel("Empty")
 	app.btnClearRecvDisplay.Connect("clicked", app.onBtnClearRecvDisplay)
 
 	btnHboxContainer.PackStart(app.btnSaveData, true, false, 0)
@@ -566,18 +557,17 @@ func (app *NetAssistantApp) doActivate(application *gtk.Application) {
 	frame1ContentBox.PackStart(app.cbPauseDisplay, false, false, 0)
 	frame1ContentBox.PackStart(btnHboxContainer, false, false, 0)
 	frame1ContentBox.SetBorderWidth(10)
-
-	// 发送设置
+	// Send Settings
 	frame2ContentBox, _ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 10)
-	app.cbAppendNewLine, _ = gtk.CheckButtonNewWithLabel("追加\\r\\n")
-	app.cbAutoCleanAfterSend, _ = gtk.CheckButtonNewWithLabel("发送完自动清空")
-	app.cbSendByHex, _ = gtk.CheckButtonNewWithLabel("按十六进制发送")
-	app.cbDataSourceCycleSend, _ = gtk.CheckButtonNewWithLabel("数据源循环发送")
+	app.cbAppendNewLine, _ = gtk.CheckButtonNewWithLabel("Append \\r\\n")
+	app.cbAutoCleanAfterSend, _ = gtk.CheckButtonNewWithLabel("Auto empty")
+	app.cbSendByHex, _ = gtk.CheckButtonNewWithLabel("Send in 16 decimal")
+	app.cbDataSourceCycleSend, _ = gtk.CheckButtonNewWithLabel("Send circularly")
 	app.entryCycleTime, _ = gtk.EntryNew()
-	app.entryCycleTime.SetPlaceholderText("间隔毫秒，默认1000")
+	app.entryCycleTime.SetPlaceholderText("default 1000(ms)")
 	btnHboxContainer2, _ := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 10)
-	app.btnLoadData, _ = gtk.ButtonNewWithLabel("加载数据")
-	app.btnClearSendDisplay, _ = gtk.ButtonNewWithLabel("清空发送")
+	app.btnLoadData, _ = gtk.ButtonNewWithLabel("Load Data")
+	app.btnClearSendDisplay, _ = gtk.ButtonNewWithLabel("Empty")
 	app.btnLoadData.Connect("clicked", app.onBtnLoadData)
 	app.btnClearSendDisplay.Connect("clicked", func() {
 		buff, _ := app.tvDataSend.GetBuffer()
@@ -594,16 +584,16 @@ func (app *NetAssistantApp) doActivate(application *gtk.Application) {
 	frame2ContentBox.PackStart(btnHboxContainer2, false, false, 0)
 	frame2ContentBox.SetBorderWidth(10)
 
-	frame1, _ := gtk.FrameNew("") // 接收设置的frame
+	frame1, _ := gtk.FrameNew("")
 	frame1.Add(frame1ContentBox)
-	frame2, _ := gtk.FrameNew("") // 发送设置的frame
+	frame2, _ := gtk.FrameNew("")
 	frame2.Add(frame2ContentBox)
-	// notebookTab.Add(label1)
+
 	notebookTab.AppendPage(frame1, label1)
 	notebookTab.AppendPage(frame2, label2)
 
-	// 右边的布局
-	titleDataReceiveArea, _ := gtk.LabelNew("数据接收区")
+	// Data Received
+	titleDataReceiveArea, _ := gtk.LabelNew("Data Received")
 	titleDataReceiveArea.SetXAlign(0)
 	windowContainerRight.PackStart(titleDataReceiveArea, false, false, 0)
 	app.swDataRec, _ = gtk.ScrolledWindowNew(nil, nil)
@@ -612,11 +602,13 @@ func (app *NetAssistantApp) doActivate(application *gtk.Application) {
 	app.tvDataReceive.SetWrapMode(gtk.WRAP_CHAR)
 	app.swDataRec.Add(app.tvDataReceive)
 	windowContainerRight.PackStart(app.swDataRec, true, true, 0)
+
+	// Local info
 	middleContainer, _ := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 10)
-	app.labelLocalAddr, _ = gtk.LabelNew("当前地址")
+	app.labelLocalAddr, _ = gtk.LabelNew("Local IP")
 	app.entryCurAddr, _ = gtk.EntryNew()
 	app.entryCurAddr.SetEditable(false)
-	app.labelLocalPort, _ = gtk.LabelNew("当前端口")
+	app.labelLocalPort, _ = gtk.LabelNew("Local Port")
 	app.entryCurPort, _ = gtk.EntryNew()
 	app.entryCurPort.SetEditable(false)
 	middleContainer.PackStart(app.labelLocalAddr, false, false, 0)
@@ -624,11 +616,12 @@ func (app *NetAssistantApp) doActivate(application *gtk.Application) {
 	middleContainer.PackStart(app.labelLocalPort, false, false, 0)
 	middleContainer.PackStart(app.entryCurPort, false, false, 0)
 	windowContainerRight.PackStart(middleContainer, false, false, 0)
+
+	// send area
 	bottomContainer, _ := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 10)
 	scrollerDataSend, _ := gtk.ScrolledWindowNew(nil, nil)
 	app.tvDataSend, _ = gtk.TextViewNew()
 	app.tvDataSend.SetWrapMode(gtk.WRAP_CHAR)
-
 	scrollerDataSend.Add(app.tvDataSend)
 	scrollerDataSend.SetSizeRequest(-1, 180)
 	boxSendBtn, _ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
@@ -640,21 +633,20 @@ func (app *NetAssistantApp) doActivate(application *gtk.Application) {
 	bottomContainer.PackEnd(boxSendBtn, false, false, 0)
 	windowContainerRight.PackStart(bottomContainer, false, false, 0)
 
-	// 最底下的布局
+	// bottom area
 	app.labelStatus, _ = gtk.LabelNew("")
-	app.labelStatus.SetMarkup(`<span>等待连接</span>`)
+	app.labelStatus.SetMarkup(`Waiting connection`)
 	windowContainerBottom.PackStart(app.labelStatus, true, false, 0)
-	app.labelSendCount, _ = gtk.LabelNew("发送计数：0")
+	app.labelSendCount, _ = gtk.LabelNew("Send Count: 0")
 	windowContainerBottom.PackStart(app.labelSendCount, true, false, 0)
-	app.labelReceveCount, _ = gtk.LabelNew("接收计数：0")
+	app.labelReceveCount, _ = gtk.LabelNew("Recv Count: 0")
 	windowContainerBottom.PackStart(app.labelReceveCount, true, false, 0)
-	app.btnCleanCount, _ = gtk.ButtonNewWithLabel("复位计数")
+	app.btnCleanCount, _ = gtk.ButtonNewWithLabel("Reset")
 	app.btnCleanCount.Connect("clicked", app.onBtnCleanCount)
 
 	windowContainerBottom.PackEnd(app.btnCleanCount, false, false, 0)
 
-	frame.Add(verticalBox)
-	app.appWindow.Add(windowContainer) // 垂直布局
+	app.appWindow.Add(windowContainer)
 
 	windowContainerLeft.PackStart(frame, false, false, 0)
 	windowContainerLeft.PackStart(notebookTab, false, false, 0)
--- go.mod
+++ go.mod
@@ -1,9 +1,8 @@
-module baloneo.gtk
+module baloneo.netassistant
 
 go 1.14
 
 require (
 	github.com/gotk3/gotk3 v0.4.0
-	golang.org/x/sys v0.0.0-20200610111108-226ff32320da
 )
 	      
